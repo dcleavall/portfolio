0c3bb087c6fbf4277eb929d7cbed329cb6884929
Derrick Cleavall <dcleavall@Derricks-Laptop.local>
2025-04-25 14:20:59 -0600

    return to normal hopefully

--- client/src/index.js
+++ client/src/index.js
@@ -1,32 +1,30 @@
-import React, { Suspense, lazy } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import CartProvider from './components/store/CartContext';
 
-// Lazy-load the Stripe Elements wrapper
-const StripeWrapper = lazy(() => import('./StripeWrapper'));
-
-const stripePromise = loadStripe('pk_live_51PoexQ00NGtbi6SbzD4GbZAtWOLlMWrQdmPbcJ5dXHNnFDoFHqCEhPKbi4JcxyiuhEtd6CMyIp5xMXzlwNM6UxXG00KEIBOu7n'); // Use your Stripe public key
+// Your Stripe public key here
+const stripePromise = loadStripe('pk_live_51PoexQ00NGtbi6SbzD4GbZAtWOLlMWrQdmPbcJ5dXHNnFDoFHqCEhPKbi4JcxyiuhEtd6CMyIp5xMXzlwNM6UxXG00KEIBOu7n');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <React.StrictMode>
     <CartProvider>
       <BrowserRouter>
-        {/* Wrap Stripe Elements in Suspense */}
-        <Suspense fallback={<div>Loading Stripe...</div>}>
-          <Elements stripe={stripePromise}>
-            <StripeWrapper>
-              <App />
-            </StripeWrapper>
-          </Elements>
-        </Suspense>
+        <Elements stripe={stripePromise}>
+          <App />
+        </Elements>
       </BrowserRouter>
     </CartProvider>
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
